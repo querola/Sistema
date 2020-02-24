f71f4514157218e5de6115839cae5a41061a5394
DesarrolloProsis <rmendoza@grupo-prosis.com>
2020-02-24 17:21:54 -0600

    Agregar archivos de proyecto.

---
+++ Sistema.Entidades/Almacen/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Sistema.Entidades.Almacen
+{
+    public class Categoria
+    {
+        public int idcategoria { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre no debe de tener más de 50 caracteres, ni menos de 3 caracteres.")]
+        public string nombre { get; set; }
+        [StringLength(256)]
+        public bool condicion { get; set; }
+        public string descripcion { get; set; }
+      
+    }
+}
